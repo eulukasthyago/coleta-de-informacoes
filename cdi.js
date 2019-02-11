1d3b9e1fe8a45af245a70168b366318b27c14fe8
Lucas Tiago <eulukasthyago@outlook.com>
2019-02-11 21:03:42 -0200

    Update

--- cdi.js
+++ cdi.js
@@ -1,10 +1,21 @@
 //if (!(document.referrer === undefined || document.referrer === null || document.referrer === "")) {
 
-	let regexp = /(\/\/.+?(\/|$))/mi;					
+	let regexp = /(\/\/.+?(\/|$))/mi;
 	let pageatual = window.location.href;
 	let deondeveveio = window.document.referrer;
 	let origem = window.location.origin;
 	let os =  window.clientInformation.platform;
+	let consult = new XMLHttpRequest();
+	consult.onload = onloadfim;
+	consult.open("GET", "https://ipapi.co/json/", true);
+	consult.send();
+
+	function onloadfim(){
+		let codejson = this.responseText;
+		sessionStorage.setItem("infopeaple", codejson);
+	}
+
+	let infopeaple = JSON.parse(sessionStorage.getItem("infopeaple"));
 
 	console.log("Pagina atual:" ,pageatual)
 	console.log("De onde veio:", deondeveveio)
@@ -13,8 +24,13 @@
 	console.log("Linguagens:", window.clientInformation.languages)
 	console.log("App Version:", window.clientInformation.appVersion)
 	console.log("Vendor:", window.clientInformation.vendor)
-	//console.log(window)
-	//console.log()
+	console.log("IP:", infopeaple.ip)
+	console.log("Provedor de Internet:", infopeaple.org)
+	console.log("Cidade do provedor:", infopeaple.region)
+	console.log("Timezone:", infopeaple.timezone)
+	console.log("UTC:", infopeaple.utc_offset)
+	console.log("Pais:", infopeaple.country_name)
+	console.log("Região:", infopeaple.region)
 	//console.log()
 
 	pageatual = regexp.exec(pageatual);
@@ -24,7 +40,7 @@
 	//if(deondeveveio[0] != pageatual[0]){
 	//	localStorage.setItem("Page", deondeveveio[0]);
 	//}else{
-	//	console.log("Page:", window.location.href);					
+	//	console.log("Page:", window.location.href);
 	//}
 
 //}
